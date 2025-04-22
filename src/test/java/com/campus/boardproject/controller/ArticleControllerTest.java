cf1171cce811244a366dd01b2c829268c8d84aef
yys <yeseul7002@gmail.com>
2025-04-22 16:20:05 +0900

    #16 - 게시글 뷰 기본 틀 작성
    컨틉롤러, 뷰, 테스트 한번에 적용

--- src/test/java/com/campus/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/campus/boardproject/controller/ArticleControllerTest.java
@@ -36,7 +36,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] - 게시글 상세페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestArticleView_thenReturnArticleView () throws Exception {
