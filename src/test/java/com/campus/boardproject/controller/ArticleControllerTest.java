395d886466fc47a5579cf0de88ef6316868e94d2
yys <yeseul7002@gmail.com>
2025-04-22 13:36:11 +0900

    #16 - 컨트롤러 테스트업데이트

--- src/test/java/com/campus/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/campus/boardproject/controller/ArticleControllerTest.java
@@ -45,7 +45,7 @@ class ArticleControllerTest {
         //when
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -60,7 +60,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
     @Disabled("구현 중")
     @DisplayName("[view][GET] - 게시글 해시태그 검색 페이지 - 정상호출")
@@ -72,7 +72,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
 }
