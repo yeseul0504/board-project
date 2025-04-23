135029244dc5444041d4905f96ee452c01519a45
yys <yeseul7002@gmail.com>
2025-04-23 18:17:07 +0900

    #18 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성

--- src/main/java/com/campus/boardproject/service/ArticleService.java
+++ src/main/java/com/campus/boardproject/service/ArticleService.java
@@ -2,9 +2,11 @@ package com.campus.boardproject.service;
 
 import com.campus.boardproject.domain.type.SearchType;
 import com.campus.boardproject.dto.ArticleDto;
+import com.campus.boardproject.dto.ArticleWithCommentsDto;
 import com.campus.boardproject.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,21 +18,22 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
     }
+
 }
