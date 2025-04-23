6b85aaca8213354ea0cefd2e740620238e2c61a2
yys <yeseul7002@gmail.com>
2025-04-23 17:53:21 +0900

    #18 - 게시글 서비스 로직의 테스트와 골격 잡기

---
+++ src/main/java/com/campus/boardproject/service/ArticleService.java
@@ -0,0 +1,36 @@
+package com.campus.boardproject.service;
+
+import com.campus.boardproject.domain.type.SearchType;
+import com.campus.boardproject.dto.ArticleDto;
+import com.campus.boardproject.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
