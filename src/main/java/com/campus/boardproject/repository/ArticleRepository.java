6902bfa27ea8888087ca696b3142d17c719fa352
yys <yeseul7002@gmail.com>
2025-04-24 10:15:00 +0900

    #18 - 도메인추가, 수정 , 레포지토리 추가 테스트 수정, data.sql 유저 테스트코드 추가
    .gitignore에 JPA Buddy 때문에 생기는 .job 폴더 무시하게 처리

--- src/main/java/com/campus/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/campus/boardproject/repository/ArticleRepository.java
@@ -4,6 +4,9 @@ import com.campus.boardproject.domain.Article;
 import com.campus.boardproject.domain.QArticle;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
@@ -13,25 +16,21 @@ import java.util.Optional;
 
 @RepositoryRestResource
 public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
     Page<Article> findByTitle(String title, Pageable pageable);
 
     @Override
-    default void customize(QuerydslBindings bindings, QArticle root){
+    default void customize(QuerydslBindings bindings, QArticle root) {
         bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
 
-    Optional<Object> findById(long Id);
-
-    void deleteById(Long articleId);
-
-    void delete(Article article);
-
-    Article saveAndFlush(Article article);
-
-    void save(Article article);
-
-    void getReferenceById(Long id);
 }
---
+++ src/main/java/com/campus/boardproject/repository/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package com.campus.boardproject.repository;
+
+import com.campus.boardproject.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+}
